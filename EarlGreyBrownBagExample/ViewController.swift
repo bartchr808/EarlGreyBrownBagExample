32d7ed6b5ead5ecd03e5a51a48d710e35ba5d522
Bart Chrzaszcz <bart.chr@gmail.com>
2018-03-06 11:38:55 -0800

    Initial Commit

---
+++ EarlGreyBrownBagExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EarlGreyBrownBagExample
+//
+//  Created by Bart Chrzaszcz on 3/6/18.
+//  Copyright © 2018 Bart Chrzaszcz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
