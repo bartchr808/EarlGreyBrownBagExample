cbf6e83046b64317a0c9225d3ca391a7135fafc1
Bart Chrzaszcz <bart.chr@gmail.com>
2018-03-06 11:52:23 -0800

    Add accessbility identifiers

--- EarlGreyBrownBagExample/ViewController.swift
+++ EarlGreyBrownBagExample/ViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var addCell: UIButton!
+
+    @IBOutlet weak var label: UILabel!
+
+    @IBOutlet weak var cantClickButton: UIButton!
+
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBOutlet weak var mySwitch: UISwitch!
+
+    @IBOutlet weak var datePicker: UIDatePicker!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        tableView.accessibilityIdentifier = "Table View"
+        addCell.accessibilityIdentifier = "Add Cell"
+        label.accessibilityIdentifier = "Label"
+        cantClickButton.accessibilityIdentifier = "Can't Click Button"
+        progressView.accessibilityIdentifier = "Progress View"
+        slider.accessibilityIdentifier = "Slider"
+        mySwitch.accessibilityIdentifier = "Switch"
+        datePicker.accessibilityIdentifier = "Date Picker"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
